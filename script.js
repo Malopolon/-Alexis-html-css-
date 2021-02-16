74c2d437e4f40ff263adbe0131f8218a5706619d
Artem <malpolon76@gmail.com>
2021-02-16 17:24:32 +0200

    add diecriptins for team members

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+let ceo = document.getElementById('founder_CEO');
+
+ceo.addEventListener('click', () => {
+  let ceoDes = document.getElementById('descCEO');
+    ceoDes.classList.toggle('active_desc');
+});
+
+ceo.addEventListener('click', () => {
+  ceo.classList.toggle('active');
+});
+
+
+
+
+let lead = document.getElementById('lead_dev');
+
+lead.addEventListener('click', () => {
+  let leadDes = document.getElementById('descLeadDev');
+    leadDes.classList.toggle('active_desc');
+});
+
+lead.addEventListener('click', () => {
+  lead.classList.toggle('active');
+});
+
+
+
+
+let design = document.getElementById('designer');
+
+design.addEventListener('click', () => {
+  let designDes = document.getElementById('descDesign');
+    designDes.classList.toggle('active_desc');
+});
+
+design.addEventListener('click', () => {
+  design.classList.toggle('active');
+});
+
+
+
+let consultant = document.getElementById('consultant');
+
+consultant.addEventListener('click', () => {
+  let concultantDes = document.getElementById('descConsultant');
+    concultantDes.classList.toggle('active_desc');
+});
+
+consultant.addEventListener('click', () => {
+    consultant.classList.toggle('active');
+});
+
+
+
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -25,9 +80,9 @@ function showSlides(n) {
     }
 
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace("active_dots", "");
     }
     
     slides[slideIndex - 1].style.display = "flex";
-    dots[slideIndex - 1].className += " active";
+    dots[slideIndex - 1].className += " active_dots";
 }
